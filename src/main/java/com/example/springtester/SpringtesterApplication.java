1f959730cb4052fccc53c561f37f9236dbd96f9a
Ahnat57 <ahnat43@gmail.com>
2020-06-11 13:59:29 -0400

    adding files

---
+++ src/main/java/com/example/springtester/SpringtesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtesterApplication.class, args);
+    }
+
+}
